5c60afb5d2d5afcfa3d2f3d9341a0bbe4bc3f188
BrittineyWashington <https://github.com>
2023-07-29 20:19:41 -0400

    second commit

---
+++ week07_labs/week07_labs.js
@@ -0,0 +1,21 @@
+//Interesting facts
+//In fact, jaguars are Panthera onca, while a panther is either a melanistic jaguar or a melanistic leopard (Panthera pardus).
+
+//Check out the chart below to learn a few of the main differences.
+
+// Panther	Jaguar
+// Size	Up to 130 pounds
+// 23 to 28 inches at the shoulder	120 to 210 pounds
+// 25 to 30 inches at the shoulder
+// Location	Africa, Asia, India, China	Central and South America
+// Habitat	Rainforests, forests, woodlands, grasslands	Deciduous forests, rainforests, wetlands, grasslands
+// Color	Black, often with the appearance of the rosette markings (characteristic of both jaguars and leopards) visible in the coat	Pale yellow or tan and covered in black spots. Rosettes on the sides have a spot in the center
+// Body shape	Slender, muscular body, more defined head	Broad forehead, a stocky body, and limbs
+// Tail length	23 to 43 inches	18 to 30 inches
+// Kill method	Bite to the throat or the back of the neck	Bite to the head, crushing the skull
+// Lifespan	12 to 17 years	12 to 15 years
+
+
+//Check out the chart below to learn a few of the main differences.
+
+
